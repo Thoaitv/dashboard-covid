194d726b4cde35c8cc9eb7d8fa8c7873443e77d8
uyenphan123 <96158346+uyenphan123@users.noreply.github.com>
2021-12-16 10:51:45 +0700

    Add files via upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,36 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { WorldWideComponent } from './world-wide/world-wide.component';
-import { RegionalComponent } from './regional/regional.component';
+import { LeafletModule } from '@asymmetrik/ngx-leaflet';
+import { StatisticsComponent } from './statistics/statistics.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { MapComponent } from './map/map.component';
+import { ChartsComponent } from './charts/charts.component';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CountUpModule } from 'ngx-countup';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    WorldWideComponent,
-    RegionalComponent
+    StatisticsComponent,
+    MapComponent,
+    ChartsComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    LeafletModule,
+    FormsModule,
+    HttpClientModule,
+    NgxChartsModule,
+    BrowserAnimationsModule,
+    CountUpModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
